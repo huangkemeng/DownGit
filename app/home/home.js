ad48316417e0bf3e27b5d8be78193d1b470f60ac
huangkemeng <huangkemeng@outlook.com>
2023-12-14 14:48:17 +0800

    Update home.js

--- app/home/home.js
+++ app/home/home.js
@@ -32,7 +32,7 @@ homeModule.config([
 
                     var templateUrl = "https?://github.com/.+/.+";
                     var downloadUrlInfix = "#/home?url=";
-                    var downloadUrlPrefix = window.location.origin +"/DownGit/"+downloadUrlInfix;
+                    var downloadUrlPrefix = window.location.origin +"/"+downloadUrlInfix;
 
                     if ($routeParams.url) {
                         $scope.url = $routeParams.url;
